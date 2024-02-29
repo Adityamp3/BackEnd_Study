26d4edd64e429d90a02747923bd5e20fca1f79a3
Aditya Raj Lodhi <dealwithaditya@gmail.com>
2024-02-29 16:09:29 +0530

    Strings in JS

---
+++ index.29Feb.js
@@ -0,0 +1,39 @@
+// let user1 = {
+//     email: "aditya@google.com",
+//     upi: "user1@ybl"
+// } 
+// let user2 = user1;
+// user2.upi = "XXXXXXXX"
+// console.log(user1);
+// console.log(user2);
+
+const repo = 50;
+const name = "   Aditya   "
+const url = "https://www.aditya%20raj.com"
+const file = "Aditya_Raj_Lodhi_2021000446"
+const vall = new String("Aditya Raj Lodhi");
+
+// console.log(name + repo+" Value");
+// console.log(`My repo count is ${name} and hello btw`)
+
+// console.log(vall.length);
+
+// console.log(vall.__proto__);
+
+// console.log(vall.toUpperCase())
+
+// console.log(vall.charAt(2))
+
+// console.log(vall.indexOf('i'))
+
+// const anotherString = vall.slice(-6, 3);
+// console.log(anotherString);
+
+// console.log(name);
+
+// console.log(name.trim())
+
+// console.log(url.replace('%20','-'))
+// console.log(url.includes('raj'))
+
+console.log(file.split('_'))
---
+++ index29Feb+.js
@@ -0,0 +1,21 @@
+const val1 = "   Aditya   "
+console.log(val1.trim())
+console.log(val1); console.log()
+
+const val2 = "NagendraHaya"
+console.log(val2.replace('gen', 'ren'))
+console.log(val2); console.log()
+
+const url = "https://www.google.com/home"
+console.log(url.includes('google'))
+console.log(url.split('/'))
+console.log(url); console.log()
+
+const val3 = "ABCDEFGHIJK"
+console.log(val3.indexOf('e'))
+console.log(val3.indexOf('E'))
+console.log(val3.charAt(6)); console.log()
+
+const val4 = "INDIA"
+console.log(val4.length)
+console.log(val4.toLowerCase())
