44240d1a12886e73eca61d772de98000f28cef25
Aditya Raj Lodhi <dealwithaditya@gmail.com>
2024-09-04 10:06:01 +0530

    Update setTimeout.js

--- setTimeout.js
+++ setTimeout.js
@@ -25,3 +25,54 @@ console.log("End");
 // Hello!
 // W
 // Z
+
+
+// VERY VERY IMPORTANT !!!!!!!!!!!
+
+function counter() {
+  for(var i=1; i<=4; i++) {
+      function z(i){
+          setTimeout(() => {
+            console.log(i);
+            }, i * 1000);
+      }
+      z(i);
+  }
+}
+counter();
+
+// Output: 
+// 1
+// 2
+// 3
+// 4
+
+function counter() {
+  for(let i=1; i<=4; i++) {
+    setTimeout(()) => {
+      console.log(i);
+    }, i * 1000);
+  }
+}
+counter();
+
+// Output: 
+// 1
+// 2
+// 3
+// 4
+
+function counter() {
+  for(var i=1; i<=4; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 1000);
+  }
+}
+counter();
+
+// Output: 
+// 4
+// 4
+// 4
+// 4
